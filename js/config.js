4a9a6f27d8ce83d2588d0e7beaab0bdd3f366c38
Sam Reid <reids@colorado.edu>
2012-12-16 19:17:37 -0700

    Updated paths to work from git submodules

--- js/config.js
+++ js/config.js
@@ -2,18 +2,18 @@ require.config( {
                     deps: ['main'],
 
                     paths: {
-                        common: '../common',
-                        phetcommon: '../../../common/phetcommon/js',
-                        phetcommon_html: '../../../common/phetcommon/html',
+                        common: 'common',
+                        phetcommon: '../common/phetcommon/js',
+                        phetcommon_html: '../common/phetcommon/html',
 
-                        easel: '../../../contrib/easel-0.5.0',
-                        image: '../../../contrib/image-0.2.1',
-                        jquery: '../../../contrib/jquery-1.8.3',
-                        'jquery.mobile': '../../../contrib/jquery.mobile-1.2.0',
-                        underscore: '../../../contrib/underscore-1.4.2',
-                        tpl: "../../../contrib/tpl-0.2",
-                        numeric: "../../../contrib/numeric-1.2.3",
-                        i18n: "../../../contrib/i18n-2.0.1-phet"
+                        easel: '../contrib/easel-0.5.0',
+                        image: '../contrib/image-0.2.1',
+                        jquery: '../contrib/jquery-1.8.3',
+                        'jquery.mobile': '../contrib/jquery.mobile-1.2.0',
+                        underscore: '../contrib/underscore-1.4.2',
+                        tpl: "../contrib/tpl-0.2",
+                        numeric: "../contrib/numeric-1.2.3",
+                        i18n: "../contrib/i18n-2.0.1-phet"
                     },
 
                     shim: {
