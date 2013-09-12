77b635aa2e7801d399120a8e7386c6ac5b537abd
samreid <reids@colorado.edu>
2013-09-11 20:18:37 -0600

    put the background in model coordinates and added model coordinates for view

--- js/energy-skate-park-basics/view/BackgroundNode.js
+++ js/energy-skate-park-basics/view/BackgroundNode.js
@@ -8,6 +8,7 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Line = require( 'SCENERY/nodes/Line' );
   var Image = require( 'SCENERY/nodes/Image' );
   var Scene = require( 'SCENERY/Scene' );
   var ScreenView = require( 'JOIST/ScreenView' );
@@ -19,8 +20,18 @@ define( function( require ) {
     this.skater = model.skater;
     Node.call( this, { renderer: 'svg' } );
 
-    this.addChild( new Rectangle( 0, 0, energySkateParkBasicsView.layoutBounds.width, energySkateParkBasicsView.layoutBounds.height, {fill: 'yellow'} ) );
+    //Wait for bounds to fill in the grass
+    this.earth = new Rectangle( 0, 0, 0, 0, {fill: '#64aa64'} );
+    this.addChild( this.earth );
+
+    this.grass = new Line( 0, 0, 0, 0, {stroke: '#03862c', lineWidth: 0.06} );
+    this.addChild( this.grass );
   }
 
-  return inherit( Node, BackgroundNode );
+  return inherit( Node, BackgroundNode, {
+    setVisibleModelBounds: function( bounds ) {
+      this.earth.setRect( bounds.minX, bounds.minY, bounds.width, 1 );
+      this.grass.setLine( bounds.minX, bounds.minY + 1, bounds.maxX, bounds.minY + 1 );
+    }
+  } );
 } );
--- js/energy-skate-park-basics/view/EnergySkateParkBasicsView.js
+++ js/energy-skate-park-basics/view/EnergySkateParkBasicsView.js
@@ -4,7 +4,6 @@ define( function( require ) {
   'use strict';
 
   // imports
-  var Bounds2 = require( 'DOT/Bounds2' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
@@ -13,6 +12,9 @@ define( function( require ) {
   var Text = require( 'SCENERY/nodes/Text' );
   var SkaterNode = require( 'ENERGY_SKATE_PARK/energy-skate-park-basics/view/SkaterNode' );
   var BackgroundNode = require( 'ENERGY_SKATE_PARK/energy-skate-park-basics/view/BackgroundNode' );
+  var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
+  var Bounds2 = require( 'DOT/Bounds2' );
+  var Vector2 = require( 'DOT/Vector2' );
 
   function EnergySkateParkBasicsView( model, mvt ) {
 
@@ -20,13 +22,45 @@ define( function( require ) {
     ScreenView.call( thisView, { renderer: 'svg' } );
 
     //The background
-    this.addChild( new BackgroundNode( model, this ) );
+    this.backgroundNode = new BackgroundNode( model, this );
+    this.addChild( this.backgroundNode );
 
     //The skater
     this.addChild( new SkaterNode( model ) );
   }
 
-  inherit( ScreenView, EnergySkateParkBasicsView );
+  inherit( ScreenView, EnergySkateParkBasicsView, {
+    layout: function( width, height ) {
+
+      var layoutScale = this.getLayoutScale( width, height );
+      //convert the view bounds (0,0,width,height) to model bounds.
+
+      //Model bounds should have same aspect ratio as layoutBounds (like iPad ratio), should have sx === sy so it doesn't stretch out
+      //nominal bounds: 768 x 504                                                                                                                                        xk
+
+      //extend the model
+      var modelWidth = 7.68;//meters
+      var modelHeight = 5.04;//meters TODO: choose this to keep a uniform aspect ratio and make sure everything fits on the screen
+//      var groundHeight = 2;
+//      var modelBounds = new Bounds2( -modelWidth / 2, 0, modelWidth / 2, modelHeight );
+//      var viewBounds = new Bounds2( 0, 0, 768, 504 );
+//      var scale = layoutScale * 6;
+//      console.log( scale );
+      var mapping = ModelViewTransform2.createSinglePointScaleInvertedYMapping( new Vector2( 0, -2 ), new Vector2( width / 2, height ), layoutScale * 40 );
+//      var visibleBounds = mapping.viewToModelX( 0 );
+      this.backgroundNode.setMatrix( mapping.getMatrix() );
+
+      //Find the visible model coordinates so that background can be fitted exactly.
+      //Note that y is inverted so model max y is view min y
+      var modelMinX = mapping.viewToModelX( 0 );
+      var modelMinY = mapping.viewToModelY( height );
+      var modelMaxX = mapping.viewToModelX( width );
+      var modelMaxY = mapping.viewToModelY( 0 );
+
+      var visibleModelBounds = new Bounds2( modelMinX, modelMinY, modelMaxX, modelMaxY );
+      this.backgroundNode.setVisibleModelBounds( visibleModelBounds );
+    }
+  } );
 
   return EnergySkateParkBasicsView;
 } );
