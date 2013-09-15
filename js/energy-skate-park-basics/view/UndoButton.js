757a74fa699fe70e325a3087ed8992ce6faf7a17
samreid <reids@colorado.edu>
2013-09-14 22:58:03 -0600

    Color code the undo button to thermal energy

--- js/energy-skate-park-basics/view/UndoButton.js
+++ js/energy-skate-park-basics/view/UndoButton.js
@@ -14,16 +14,18 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Shape = require( 'KITE/Shape' );
+  var EnergySkateParkColorScheme = require( 'ENERGY_SKATE_PARK/energy-skate-park-basics/view/EnergySkateParkColorScheme' );
+  var Color = require( 'SCENERY/util/Color' );
 
   function UndoButton( callback, skater, options ) {
 
     options = _.extend( { cursor: 'pointer' }, options );
-    var icon = new FontAwesomeNode( 'undo', { fill: 'black', scale: 0.4 } );
+    var icon = new FontAwesomeNode( 'undo', { fill: 'white', scale: 0.4 } );
     skater.toDerivedProperty( ['thermalEnergy'],function( thermalEnergy ) {return thermalEnergy > 0;} ).link( function( hasThermalEnergy ) {
-      icon.fill = hasThermalEnergy ? 'black' : 'gray';
+      icon.fill = hasThermalEnergy ? 'white' : 'lightGray';
     } );
 
-    RectangleButton.call( this, icon, callback );
+    RectangleButton.call( this, icon, callback, {rectangleFillUp: new Color( 255, 85, 0 )} );
     this.mouseArea = this.touchArea = Shape.rectangle( icon.bounds.minX, icon.bounds.minY, icon.bounds.width, icon.bounds.height );
     this.addChild( icon );
     this.mutate( options );
