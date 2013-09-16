736f17dd880b8a02cc64fc91a6b470a8f4240ec7
samreid <reids@colorado.edu>
2013-09-16 11:02:14 -0600

    replaced fontsize with phetfont

--- js/energy-skate-park-basics/view/PlaybackSpeedControl.js
+++ js/energy-skate-park-basics/view/PlaybackSpeedControl.js
@@ -11,11 +11,12 @@ define( function( require ) {
   var VBox = require( 'SCENERY/nodes/VBox' );
   var AquaRadioButton = require( 'SUN/AquaRadioButton' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
 
   function PlayPauseControlPanel( model, options ) {
     VBox.call( this, {align: 'left', spacing: 4, children: [
-      new AquaRadioButton( model.speedProperty, 'normal', new Text( 'Normal', {fontSize: 15} ), {radius: 12, x: 130} ),
-      new AquaRadioButton( model.speedProperty, 'slow', new Text( 'Slow Motion', {fontSize: 15} ), {radius: 12} )
+      new AquaRadioButton( model.speedProperty, 'normal', new Text( 'Normal', {font: new PhetFont( 15 )} ), {radius: 12, x: 130} ),
+      new AquaRadioButton( model.speedProperty, 'slow', new Text( 'Slow Motion', {font: new PhetFont( 15 )} ), {radius: 12} )
     ]} );
   }
 
