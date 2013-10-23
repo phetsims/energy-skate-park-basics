d9792b288b2fca74284d968485c621ee6c7c8654
Chris Malley <cmalley@pixelzoom.com>
2013-10-23 13:16:16 -0600

    joist#59: replace duck-typing with instantiation or subtyping of joist.Screen, so that we can add 'navbar icon' functionality to Screen.

--- js/EnergySkateParkBasicsScreen.js
+++ js/EnergySkateParkBasicsScreen.js
@@ -6,15 +6,17 @@ define( function( require ) {
   var EnergySkateParkBasicsModel = require( 'ENERGY_SKATE_PARK_BASICS/model/EnergySkateParkBasicsModel' );
   var EnergySkateParkBasicsView = require( 'ENERGY_SKATE_PARK_BASICS/view/EnergySkateParkBasicsView' );
   var Image = require( 'SCENERY/nodes/Image' );
+  var inherit = require( 'PHET_CORE/inherit' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Screen = require( 'JOIST/Screen' );
   var Vector2 = require( 'DOT/Vector2' );
 
   function EnergySkateParkBasicsScreen( name, icon, draggableTracks, friction ) {
-    this.name = name;
-    this.icon = new Image( icon );
-    this.createModel = function() { return new EnergySkateParkBasicsModel( draggableTracks, friction ); };
-    this.createView = function( model ) { return new EnergySkateParkBasicsView( model ); };
+    Screen.call( this, name, new Image( icon ),
+      function() { return new EnergySkateParkBasicsModel( draggableTracks, friction ); },
+      function( model ) { return new EnergySkateParkBasicsView( model ); }
+    );
   }
 
-  return EnergySkateParkBasicsScreen;
+  return inherit( Screen, EnergySkateParkBasicsScreen );
 } );
