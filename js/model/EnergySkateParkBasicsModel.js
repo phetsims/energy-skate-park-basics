eb5992d1e1dda4d138bff230393369f8e341c3e5
samreid <reids@colorado.edu>
2013-11-18 14:27:08 -0700

    fixed typo

--- js/model/EnergySkateParkBasicsModel.js
+++ js/model/EnergySkateParkBasicsModel.js
@@ -302,7 +302,7 @@ define( function( require ) {
       var gravity = skater.gravity;
 
       //P means Prime (i.e. derivative with respect to time)
-      //D means Dot (i.e. derivative with repsect to x)
+      //D means Dot (i.e. derivative with respect to x)
       //2 means after the step
       var xP = track.xSplineDiff.at( u );
       var yP = track.ySplineDiff.at( u );
