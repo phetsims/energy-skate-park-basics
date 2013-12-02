7c4a6b2f63b65a4620b4faf12eef44404d55468e
samreid <reids@colorado.edu>
2013-12-02 14:35:47 -0700

    added a monkey patch for numeric.js to improve performance

--- js/EnergySkateParkBasicsScreen.js
+++ js/EnergySkateParkBasicsScreen.js
@@ -17,11 +17,33 @@ define( function( require ) {
   var Vector2 = require( 'DOT/Vector2' );
 
   function EnergySkateParkBasicsScreen( name, icon, draggableTracks, friction ) {
+
+    this.monkeyPatchNumeric();
+
     Screen.call( this, name, new Image( icon ),
       function() { return new EnergySkateParkBasicsModel( draggableTracks, friction ); },
       function( model ) { return new EnergySkateParkBasicsView( model ); }
     );
   }
 
-  return inherit( Screen, EnergySkateParkBasicsScreen );
+  return inherit( Screen, EnergySkateParkBasicsScreen, {monkeyPatchNumeric: function() {
+
+    //The most important function in numeric.js is just too slow because it uses tensor versions of all functions.
+    //This version inlines everything.
+    numeric.Spline.prototype._at = function _at( x1, p ) {
+      var x = this.x;
+      var yl = this.yl;
+      var yr = this.yr;
+      var kl = this.kl;
+      var kr = this.kr;
+      var a, b, t;
+      a = (kl[p] * (x[p + 1] - x[p])) - (yr[p + 1] - yl[p]);
+      b = kr[p + 1] * (x[p] - x[p + 1]) + yr[p + 1] - yl[p];
+      t = (x1 - x[p]) / (x[p + 1] - x[p]);
+      var s = t * (1 - t);
+      return ((1 - t) * yl[p] + t * yr[p + 1] +
+              a * s * (1 - t) ) +
+             b * s * t;
+    };
+  }} );
 } );
