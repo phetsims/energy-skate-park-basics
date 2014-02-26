b859bd08b1e8fcc2925b44e0acfc7bfc9d75dc06
Sam Reid <reids@colorado.edu>
2014-02-26 12:20:29 -0700

    Converted to a raster trash can icon, modified from font awesome, see #80

--- js/view/ClearThermalButton.js
+++ js/view/ClearThermalButton.js
@@ -13,10 +13,12 @@ define( function( require ) {
   var Shape = require( 'KITE/Shape' );
   var Color = require( 'SCENERY/util/Color' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var Image = require( 'SCENERY/nodes/Image' );
   var Path = require( 'SCENERY/nodes/Path' );
-  var Line = require( 'SCENERY/nodes/Line' );
   var Vector2 = require( 'DOT/Vector2' );
   var RoundShinyButton = require( 'SCENERY_PHET/RoundShinyButton' );
+  var trashCanImage = require( 'image!ENERGY_SKATE_PARK_BASICS/trash-can-open.png' );
+  var trashCanGrayImage = require( 'image!ENERGY_SKATE_PARK_BASICS/trash-can-open-gray.png' );
 
   function ClearThermalButton( callback, skater, options ) {
     options = _.extend( { cursor: 'pointer' }, options );
@@ -42,69 +44,18 @@ define( function( require ) {
 
     var arrowTail = new Path( new Shape().moveTo( 0, 0 ).quadraticCurveTo( xControl, yControl, xTip, yTip ), { stroke: 'black', lineWidth: 3 } );
 
-    var arrowhead = createArrowhead( Math.PI / 3, new Vector2( xTip, yTip ) ).mutate( {x: -18, y: -16} );
+    var arrowhead = createArrowhead( Math.PI / 3, new Vector2( xTip, yTip ) );
 
-    var lidVector = Vector2.createPolar( 20, -Math.PI / 4 );
-    var lidStart = new Vector2( 22, 0 );
-    var lidEnd = lidStart.plus( lidVector );
-    var handleWidth = 8;
-    var handleStart = lidStart.plus( lidVector.times( 0.5 ).minus( lidVector.normalized().times( -handleWidth / 2 ) ) );
-    var handleEnd = lidStart.plus( lidVector.times( 0.5 ).minus( lidVector.normalized().times( handleWidth / 2 ) ) );
-
+    var offset = {x: -18, y: 2};
+    var image = new Image( trashCanImage, {scale: 0.3} );
     var icon = new Node( {scale: 0.5, x: -0.125, children: [
-      new Path( new Shape().moveTo( 0, 0 ).lineTo( 20, 0 ).lineTo( 20, 24 ).lineTo( 0, 24 ).close(), {stroke: 'black', lineWidth: 1} ),
-      new Line( 4, 3, 4, 21, {lineWidth: 1, stroke: 'black'} ),
-      new Line( 10, 3, 10, 21, {lineWidth: 1, stroke: 'black'} ),
-      new Line( 16, 3, 16, 21, {lineWidth: 1, stroke: 'black'} ),
-      //Lid
-      new Line( lidStart.x, lidStart.y, lidEnd.x, lidEnd.y, {lineWidth: 2, stroke: 'black'} ),
-
-      //Handle
-      new Line( handleStart.x + 1, handleStart.y + 1, handleEnd.x + 1, handleEnd.y + 1, {lineWidth: 2, stroke: 'black'} ),
-      arrowTail.mutate( {x: -18, y: -16} ),
-      arrowhead
+      image,
+      arrowTail.mutate( offset ),
+      arrowhead.mutate( offset )
     ]} );
 
-    //experiment with pixel perfect coordinates
-//    var isRounded = function( x ) { return x === Math.floor( x ); };
-//    setInterval( function() {
-//      var sx = icon.getLocalToGlobalMatrix().entries[0];
-//      var x = icon.getLocalToGlobalMatrix().entries[6];
-//      if ( sx !== 1 || !isRounded( x ) ) {
-//        icon.setScaleMagnitude( 1 );
-//        icon.x = 0;
-//        var scaling = icon.getLocalToGlobalMatrix().entries[0];
-//        icon.setScaleMagnitude( 1.0 / scaling );
-//
-//        var currentX = icon.getLocalToGlobalMatrix().entries[6];
-//        var newScaling = icon.getLocalToGlobalMatrix().entries[0];
-//        var desiredX = Math.floor( currentX );
-//        var distance = (desiredX - currentX) * newScaling;
-//        console.log( x, desiredX, distance );
-//        icon.x = icon.x + distance;
-//
-//        console.log( 'after update:', icon.getLocalToGlobalMatrix().entries[0], icon.getLocalToGlobalMatrix().entries[6] );
-////        var currentX = icon.getLocalToGlobalMatrix().entries[6];
-////        var newScaling = icon.getLocalToGlobalMatrix().entries[0];
-//      }
-//
-////      var y = icon.getLocalToGlobalMatrix().entries[7];
-////      var s2 = icon.getLocalToGlobalMatrix().entries[0];
-//////      console.log( icon.getLocalToGlobalMatrix() );
-////      console.log( sx, x, y, s2 );
-////
-//////      if ( x === 361.46145448047844 ) {
-//////        clearThermalButton.translate( -0.46145448047844, -0.75 );
-//////      }
-////      260.90450849718746 82.75
-//    }, 3000 );
-
     skater.toDerivedProperty( ['thermalEnergy'],function( thermalEnergy ) {return thermalEnergy > 0;} ).link( function( hasThermalEnergy ) {
-      for ( var i = 0; i < icon.children.length; i++ ) {
-        var child = icon.children[i];
-        child.stroke = hasThermalEnergy ? 'black' : 'gray';
-      }
-      icon.children[0].fill = hasThermalEnergy ? 'lightgray' : null;
+      image.image = hasThermalEnergy ? trashCanImage : trashCanGrayImage;
       arrowhead.fill = hasThermalEnergy ? new Color( 255, 85, 0 ) : 'gray';
       arrowTail.stroke = hasThermalEnergy ? new Color( 255, 85, 0 ) : 'gray';
       arrowhead.stroke = null;
