3ed979b54dc990f9b45749ab3ca0f0638be4e0a9
Sam Reid <reids@colorado.edu>
2014-03-05 15:15:12 -0700

    reverted to 'svg' renderer

--- js/view/EnergySkateParkBasicsView.js
+++ js/view/EnergySkateParkBasicsView.js
@@ -48,7 +48,7 @@ define( function( require ) {
   function EnergySkateParkBasicsView( model ) {
 
     var view = this;
-    ScreenView.call( view, { renderer: 'canvas' } );
+    ScreenView.call( view, { renderer: 'svg' } );
 
     var modelPoint = new Vector2( 0, 0 );
     var viewPoint = new Vector2( this.layoutBounds.width / 2, this.layoutBounds.height - BackgroundNode.earthHeight );//earth is 70px high in stage coordinates
