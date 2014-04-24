e25903d927e0d17934a83e3c53e98ef05e2d9e06
Sam Reid <reids@colorado.edu>
2014-04-23 21:01:55 -0600

    Restore the skater's track when "return skater" is pressed, see #126

--- js/model/EnergySkateParkBasicsModel.js
+++ js/model/EnergySkateParkBasicsModel.js
@@ -717,8 +717,17 @@ define( function( require ) {
 
     //Return to the place he was last released by the user.  Also restores the track the skater was on so the initial conditions are the same as the previous release
     returnSkater: function() {
-      if ( this.skater.startingTrack && this.skater.startingTrack.scene !== undefined ) {
-        this.scene = this.skater.startingTrack.scene;
+      if ( this.skater.startingTrack ) {
+
+        //Restore the starting track if in one of the scenes
+        if ( this.skater.startingTrack.scene !== undefined ) {
+          this.scene = this.skater.startingTrack.scene;
+        }
+
+        //Restore the skater's track, see #126
+        else {
+          this.tracks.add( this.skater.startingTrack );
+        }
       }
       this.skater.returnSkater();
     },
