0225b5c854d1f414228d4f82e08cba0a9c57e752
Sam Reid <reids@colorado.edu>
2014-08-23 15:31:23 -0600

    updated docs

--- js/model/SkaterState.js
+++ js/model/SkaterState.js
@@ -4,6 +4,7 @@
  * Immutable snapshot of skater state for updating the physics. To improve performance, operate solely on a skaterState instance without updating the real skater,
  * so that the skater model itself can be set only once, and trigger callbacks only once (no matter how many subdivisions).
  * This can also facilitate debugging and ensuring energy is conserved from one step to another. Pooled to avoid allocation problems, see #50
+ * Another reason this class is valuable is to create and evaluate proposed states before applying them to the live model.
  *
  * @author Sam Reid
  */
