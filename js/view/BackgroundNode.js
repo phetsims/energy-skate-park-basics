c211a8c6ec7aa5dc0b82dd6898dbad49c8d2496b
Chris Malley <cmalley@pixelzoom.com>
2014-09-15 10:58:01 -0600

    phetsims/phet-core#11 @param {Options} [options]

--- js/view/BackgroundNode.js
+++ js/view/BackgroundNode.js
@@ -22,7 +22,7 @@ define( function( require ) {
 
   /**
    * @param {Bounds2} layoutBounds the ScreenView layoutBounds
-   * @param {Object} options
+   * @param {Object} [options]
    * @constructor
    */
   function BackgroundNode( layoutBounds, options ) {
