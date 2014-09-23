35f76becccf78ce08a52cdeace29ce6d37ca592c
Sam Reid <reids@colorado.edu>
2014-09-23 12:07:53 -0600

    Added pie chart in WebGLNode

--- js/view/PieChartWebGLNode.js
+++ js/view/PieChartWebGLNode.js
@@ -15,14 +15,19 @@ define( function( require ) {
   var Matrix4 = require( 'DOT/Matrix4' );
   var WebGLLayer = require( 'SCENERY/layers/WebGLLayer' );
   var Color = require( 'SCENERY/util/Color' );
+  var DerivedProperty = require( 'AXON/DerivedProperty' );
 
   /**
    * @param {SingleBulbModel} model
    * @param {Object} [options], must contain a canvasBounds attribute of type Bounds2
    * @constructor
    */
-  function PieChartWebGLNode( model, options ) {
+  function PieChartWebGLNode( model, color, radiusProperty, startAngleProperty, extentProperty, options ) {
 
+    this.color = color;
+    this.radiusProperty = radiusProperty;
+    this.startAngleProperty = startAngleProperty;
+    this.extentProperty = extentProperty;
     WebGLNode.call( this, {canvasBounds: new Bounds2( 0, 0, 100, 100 )} );
 
     this.invalidatePaint();
@@ -35,16 +40,16 @@ define( function( require ) {
       this.buffer = gl.createBuffer();
       gl.bindBuffer( gl.ARRAY_BUFFER, this.buffer );
 
-      var centerX = 0.5;
-      var centerY = 0.5;
+      var centerX = 0;
+      var centerY = 0;
       var radius = 0.5;
 
       //40 seemed a little aliased.  Anything 50+ seems pretty great.  Though even 1000 samples doesn't slow performance
-      var numSamples = 50;
+      var numSamples = 200;
 
       var vertices = [centerX, centerY];
       for ( var i = 0; i <= numSamples; i++ ) {
-        var angle = Math.PI * 2 / numSamples * i;
+        var angle = -Math.PI * 2 / numSamples * i;
         var x = radius * Math.cos( angle ) + centerX;
         var y = radius * Math.sin( angle ) + centerY;
         vertices.push( x );
@@ -63,31 +68,41 @@ define( function( require ) {
     updateRectangle: function() {
     },
 
-    testXValue: 0,
+    myPosition: 0,
+    myAngle: 0,
     render: function( gl, shaderProgram, viewMatrix ) {
 
-      var rectangleX = this.testXValue++;
+      var extent = this.extentProperty.value;
+      var radius = this.radiusProperty.value;
+      var startAngle = this.startAngleProperty.value;
+
+      var rectangleX = 0;
       var rectangleY = 0;
-      var rectangleWidth = 100;
-      var rectangleHeight = 100;
+      var rectangleWidth = radius * 2;
+      var rectangleHeight = radius * 2;
 
       var rectangleOffset = Matrix4.translation( rectangleX, rectangleY, 0 );
       var rectangleSize = Matrix4.scaling( rectangleWidth, rectangleHeight, 1 );
-      var uMatrix = viewMatrix.timesMatrix( rectangleOffset.timesMatrix( rectangleSize ) );
+      var rotationMatrix = Matrix4.rotationZ( -startAngle );
+      var uMatrix = viewMatrix.timesMatrix( rectangleOffset.timesMatrix( rectangleSize.timesMatrix( rotationMatrix ) ) );
 
       // combine image matrix (to scale aspect ratios), the trail's matrix, and the matrix to device coordinates
       gl.uniformMatrix4fv( shaderProgram.uniformLocations.uMatrix, false, uMatrix.entries );
 
       //Indicate the branch of logic to use in the ubershader.  In this case, a texture should be used for the image
       gl.uniform1i( shaderProgram.uniformLocations.uFragmentType, WebGLLayer.fragmentTypeFill );
-      var color = Color.toColor( 'blue' );
+      var color = Color.toColor( this.color );
       gl.uniform4f( shaderProgram.uniformLocations.uColor, color.r / 255, color.g / 255, color.b / 255, color.a );
 
       gl.bindBuffer( gl.ARRAY_BUFFER, this.buffer );
       gl.vertexAttribPointer( shaderProgram.attributeLocations.aVertex, 2, gl.FLOAT, false, 0, 0 );
 
       //To cut out a piece from the pie, just select the appropriate start/end vertices, then the call is still static.
-      gl.drawArrays( gl.TRIANGLE_FAN, 0, this.vertices.length / 2 );
+      var numPoints = this.vertices.length / 2;
+
+      var numOuterSamples = numPoints - 1;
+      var numToDraw = Math.round( numOuterSamples * extent / Math.PI / 2 );
+      gl.drawArrays( gl.TRIANGLE_FAN, 0, numToDraw );
     },
 
     //TODO: Is this necessary?
