5193e18fdd101e38667565bdf22e66f0c56e87ea
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-02-25 12:41:55 -0700

    Remove "valueEquals" method from Property API, see https://github.com/phetsims/axon/issues/39

--- js/energy-skate-park-basics/view/EnergySkateParkBasicsScreenView.js
+++ js/energy-skate-park-basics/view/EnergySkateParkBasicsScreenView.js
@@ -93,7 +93,8 @@ define( function( require ) {
 
     // For the playground screen, show attach/detach toggle buttons
     if ( model.draggableTracks ) {
-      var property = model.draggableTracks ? new Property( true ) : model.property( 'scene' ).valueEquals( 2 );
+      var property = model.draggableTracks ? new Property( true ) :
+                     new DerivedProperty( [model.property( 'scene' ) ], function( scene ) { return scene === 2; } );
       this.attachDetachToggleButtons = new AttachDetachToggleButtons( model.property( 'detachable' ), property, this.controlPanel.contentWidth, {
         top: this.controlPanel.bottom + 5,
         centerX: this.controlPanel.centerX
