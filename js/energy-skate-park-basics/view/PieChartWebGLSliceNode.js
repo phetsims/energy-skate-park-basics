8186916b52b6ab8d3a7fae3eae41997b3cc049e6
samreid <reids@colorado.edu>
2016-02-23 15:22:34 -0700

    Got example WebGLNode code working, see #313

--- js/energy-skate-park-basics/view/PieChartWebGLSliceNode.js
+++ js/energy-skate-park-basics/view/PieChartWebGLSliceNode.js
@@ -14,6 +14,8 @@ define( function( require ) {
   var Bounds2 = require( 'DOT/Bounds2' );
   var Matrix4 = require( 'DOT/Matrix4' );
   var Color = require( 'SCENERY/util/Color' );
+  var ShaderProgram = require( 'SCENERY/util/ShaderProgram' );
+  var Shape = require( 'KITE/Shape' );
 
   // Hack workaround to satisfy lint until WebGL support is restored
   var WebGLLayer = null;
@@ -36,9 +38,92 @@ define( function( require ) {
     WebGLNode.call( this, { canvasBounds: new Bounds2( 0, 0, 100, 100 ) } );
 
     this.invalidatePaint();
+
+    this.shape = Shape.regularPolygon( 3, 100 * Math.sqrt( 2 ) );
   }
 
   return inherit( WebGLNode, PieChartWebGLSliceNode, {
+
+    initializeWebGLDrawable: function( drawable ) {
+      console.log( 'hello' );
+      var gl = drawable.gl;
+
+      // Simple example for custom shader
+      var vertexShaderSource = [
+        // Position
+        'attribute vec3 aPosition;',
+        'attribute vec3 aColor;',
+        'varying vec3 vColor;',
+        'uniform mat3 uModelViewMatrix;',
+        'uniform mat3 uProjectionMatrix;',
+
+        'void main( void ) {',
+        '  vColor = aColor;',
+        // homogeneous model-view transformation
+        '  vec3 view = uModelViewMatrix * vec3( aPosition.xy, 1 );',
+        // homogeneous map to to normalized device coordinates
+        '  vec3 ndc = uProjectionMatrix * vec3( view.xy, 1 );',
+        // combine with the z coordinate specified
+        '  gl_Position = vec4( ndc.xy, aPosition.z, 1.0 );',
+        '}'
+      ].join( '\n' );
+
+      // Simple demo for custom shader
+      var fragmentShaderSource = [
+        'precision mediump float;',
+        'varying vec3 vColor;',
+
+        // Returns the color from the vertex shader
+        'void main( void ) {',
+        '  gl_FragColor = vec4( vColor, 1.0 );',
+        '}'
+      ].join( '\n' );
+
+      drawable.shaderProgram = new ShaderProgram( gl, vertexShaderSource, fragmentShaderSource, {
+        attributes: [ 'aPosition', 'aColor' ],
+        uniforms: [ 'uModelViewMatrix', 'uProjectionMatrix' ]
+      } );
+
+      drawable.vertexBuffer = gl.createBuffer();
+
+      var points = this.shape.subpaths[ 0 ].points;
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.vertexBuffer );
+      gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( [
+        points[ 0 ].x, points[ 0 ].y, 0.2,
+        points[ 1 ].x, points[ 1 ].y, 0.2,
+        points[ 2 ].x, points[ 2 ].y, 0.2
+      ] ), gl.STATIC_DRAW );
+
+      drawable.colorBuffer = gl.createBuffer();
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.colorBuffer );
+      gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( [
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1
+      ] ), gl.STATIC_DRAW );
+
+    },
+    paintWebGLDrawable: function( drawable, matrix ) {
+      var gl = drawable.gl;
+      var shaderProgram = drawable.shaderProgram;
+
+      shaderProgram.use();
+
+      gl.uniformMatrix3fv( shaderProgram.uniformLocations.uModelViewMatrix, false, matrix.entries );
+      gl.uniformMatrix3fv( shaderProgram.uniformLocations.uProjectionMatrix, false, drawable.webGLBlock.projectionMatrixArray );
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.vertexBuffer );
+      gl.vertexAttribPointer( shaderProgram.attributeLocations.aPosition, 3, gl.FLOAT, false, 0, 0 );
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.colorBuffer );
+      gl.vertexAttribPointer( shaderProgram.attributeLocations.aColor, 3, gl.FLOAT, false, 0, 0 );
+
+      gl.drawArrays( gl.TRIANGLES, 0, 3 );
+
+      shaderProgram.unuse();
+
+    },
 
     initialize: function( gl ) {
 
