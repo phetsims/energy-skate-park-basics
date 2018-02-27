8e0339c0ae60e6af9d401874f11e4a93a58fbcf7
samreid <reids@colorado.edu>
2018-02-27 16:15:09 -0700

    Cleaned up require statements, see https://github.com/phetsims/phet-io/issues/1307

--- js/energy-skate-park-basics/model/ControlPoint.js
+++ js/energy-skate-park-basics/model/ControlPoint.js
@@ -19,9 +19,9 @@ define( function( require ) {
   var Tandem = require( 'TANDEM/Tandem' );
   var Vector2 = require( 'DOT/Vector2' );
   var Vector2IO = require( 'DOT/Vector2IO' );
+  var ControlPointIO = require( 'ENERGY_SKATE_PARK_BASICS/energy-skate-park-basics/model/ControlPointIO' );
 
   // phet-io modules
-  var ControlPointIO = require( 'ENERGY_SKATE_PARK_BASICS/energy-skate-park-basics/model/ControlPointIO' );
   var NullableIO = require( 'ifphetio!PHET_IO/types/NullableIO' );
 
   /**
