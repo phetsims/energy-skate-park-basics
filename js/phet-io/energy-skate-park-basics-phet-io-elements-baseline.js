45d0953d60b555302988535ae9af77ad6558c71a
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:57:58 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/energy-skate-park-basics-phet-io-elements-baseline.js
+++ js/phet-io/energy-skate-park-basics-phet-io-elements-baseline.js
@@ -10142,7 +10142,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "energySkateParkBasics.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
