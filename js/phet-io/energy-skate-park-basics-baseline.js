268d42f4e1942267252cf8d14a8b3f06d66a8e6a
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:00 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/energy-skate-park-basics-baseline.js
+++ js/phet-io/energy-skate-park-basics-baseline.js
@@ -9859,7 +9859,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "energySkateParkBasics.general.view.simNameProperty": {
+    "energySkateParkBasics.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
