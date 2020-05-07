6a3fc94b6d9d73bd054e6dd51249c11ed41fba6c
zepumph <michael.kauzmann@colorado.edu>
2020-05-07 12:11:40 -0800

    Convert SimLauncher to es6 class, rename to simLauncher.js, https://github.com/phetsims/joist/issues/630

--- js/energy-skate-park-basics-main.js
+++ js/energy-skate-park-basics-main.js
@@ -6,13 +6,13 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import SimLauncher from '../../joist/js/SimLauncher.js';
+import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import EnergySkateParkBasicsSim from './energy-skate-park-basics/EnergySkateParkBasicsSim.js';
 
 // constants
 const tandem = Tandem.ROOT;
 
-SimLauncher.launch( () => {
+simLauncher.launch( () => {
   new EnergySkateParkBasicsSim( tandem ).start();
 } );
