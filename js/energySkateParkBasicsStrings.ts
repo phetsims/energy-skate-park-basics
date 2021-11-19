697ad7d4ad33fd3212119f14799560fd70c0cf28
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:28 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/energySkateParkBasicsStrings.ts
@@ -0,0 +1,58 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import energySkateParkBasics from './energySkateParkBasics.js';
+
+type StringsType = {
+  'properties': {
+    'speed': string
+  },
+  'small': string,
+  'screen': {
+    'trackPlayground': string,
+    'friction': string,
+    'introduction': string
+  },
+  'controls': {
+    'mass': string,
+    'show-grid': string,
+    'gravity': {
+      'lots': string,
+      'none': string
+    },
+    'friction': {
+      'title': string
+    },
+    'restart-skater': string
+  },
+  'energy-skate-park-basics': {
+    'title': string
+  },
+  'slow': {
+    'motion': string
+  },
+  'energy': {
+    'thermal': string,
+    'total': string,
+    'kinetic': string,
+    'potential': string,
+    'energy': string
+  },
+  'plots': {
+    'bar-graph': string
+  },
+  'pieChart': string,
+  'large': string,
+  'normal': string,
+  'zeroMeters': string
+};
+
+const energySkateParkBasicsStrings = getStringModule( 'ENERGY_SKATE_PARK_BASICS' ) as StringsType;
+
+energySkateParkBasics.register( 'energySkateParkBasicsStrings', energySkateParkBasicsStrings );
+
+export default energySkateParkBasicsStrings;
