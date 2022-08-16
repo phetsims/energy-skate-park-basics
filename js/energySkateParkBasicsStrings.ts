9b96c14f181f90c097ca33042bb685c00d1f4b4c
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:33 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/energySkateParkBasicsStrings.ts
+++ js/energySkateParkBasicsStrings.ts
@@ -5,50 +5,74 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import energySkateParkBasics from './energySkateParkBasics.js';
 
 type StringsType = {
   'properties': {
     'speed': string;
+    'speedProperty': TReadOnlyProperty<string>;
   };
   'small': string;
+  'smallProperty': TReadOnlyProperty<string>;
   'screen': {
     'trackPlayground': string;
+    'trackPlaygroundProperty': TReadOnlyProperty<string>;
     'friction': string;
+    'frictionProperty': TReadOnlyProperty<string>;
     'introduction': string;
+    'introductionProperty': TReadOnlyProperty<string>;
   };
   'controls': {
     'mass': string;
+    'massProperty': TReadOnlyProperty<string>;
     'show-grid': string;
+    'show-gridProperty': TReadOnlyProperty<string>;
     'gravity': {
       'lots': string;
+      'lotsProperty': TReadOnlyProperty<string>;
       'none': string;
+      'noneProperty': TReadOnlyProperty<string>;
     };
     'friction': {
       'title': string;
+      'titleProperty': TReadOnlyProperty<string>;
     };
     'restart-skater': string;
+    'restart-skaterProperty': TReadOnlyProperty<string>;
   };
   'energy-skate-park-basics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'slow': {
     'motion': string;
+    'motionProperty': TReadOnlyProperty<string>;
   };
   'energy': {
     'thermal': string;
+    'thermalProperty': TReadOnlyProperty<string>;
     'total': string;
+    'totalProperty': TReadOnlyProperty<string>;
     'kinetic': string;
+    'kineticProperty': TReadOnlyProperty<string>;
     'potential': string;
+    'potentialProperty': TReadOnlyProperty<string>;
     'energy': string;
+    'energyProperty': TReadOnlyProperty<string>;
   };
   'plots': {
     'bar-graph': string;
+    'bar-graphProperty': TReadOnlyProperty<string>;
   };
   'pieChart': string;
+  'pieChartProperty': TReadOnlyProperty<string>;
   'large': string;
+  'largeProperty': TReadOnlyProperty<string>;
   'normal': string;
+  'normalProperty': TReadOnlyProperty<string>;
   'zeroMeters': string;
+  'zeroMetersProperty': TReadOnlyProperty<string>;
 };
 
 const energySkateParkBasicsStrings = getStringModule( 'ENERGY_SKATE_PARK_BASICS' ) as StringsType;
